c9090ed2407e7fe9ac75023e522382e7e29bb2b2
Павел Филиппов <pgphil@yandex.ru>
2023-05-12 11:04:47 +0300

    General url changes.

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -13,8 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import include, path, reverse_lazy
+from django.views.generic.edit import CreateView
 
 handler404 = 'pages.views.error_404'
 handler500 = 'pages.views.error_500'
@@ -23,4 +27,21 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include('pages.urls', namespace='pages')),
     path('', include('blog.urls', namespace='index')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('blog: index'),
+        ),
+        name='registration',
+    ),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+        )
